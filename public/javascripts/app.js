88282370635daa320f09d86d18602657d21f88f4
Jens Richnow <jens.richnow@adscale.co.nz>
2014-05-22 20:50:55 +1200

    getting movies as Json file from server.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,12 +1,15 @@
 var mediaApp = angular.module('mediaApp', []);
 
-mediaApp.controller('MovieListCtrl', function ($scope) {
-	$scope.movies = [
-	                 {'title': 'Gone with the Wind', 'genre':'Romance, Drama', 'year':1939},
-	                 {'title': 'The Matrix', 'genre':'Action, Sci-Fi', 'year':1999},
-	                 {'title': 'Kill Bill: Vol. 1', 'genre':'Action, Crime', 'year':2003},
-	                 {'title': 'The Bourne Identity', 'genre':'Action, Adventure, Mystery', 'year':2002}
-	                 ];
-	
+mediaApp.controller('MovieListCtrl', function ($scope, $http) {
+	$http.get('assets/movies.json').success(function(data) {
+	    $scope.movies = data;
+	  });
+//	$scope.movies = [
+//	                 {'title': 'Gone with the Wind', 'genre':'Romance, Drama', 'year':1939},
+//	                 {'title': 'The Matrix', 'genre':'Action, Sci-Fi', 'year':1999},
+//	                 {'title': 'Kill Bill: Vol. 1', 'genre':'Action, Crime', 'year':2003},
+//	                 {'title': 'The Bourne Identity', 'genre':'Action, Adventure, Mystery', 'year':2002}
+//	                 ];
+
 	$scope.movieProp = 'title';
 }); 
