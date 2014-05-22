970666bc955d681140a908c42b4ea5c1c9cd2cde
Jens Richnow <jens.richnow@adscale.co.nz>
2014-05-22 22:37:41 +1200

    sorting and pagination for movie table.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,15 +1,26 @@
-var mediaApp = angular.module('mediaApp', []);
+var mediaApp = angular.module('mediaApp', [ 'ngTable' ]);
 
-mediaApp.controller('MovieListCtrl', function ($scope, $http) {
-	$http.get('assets/movies.json').success(function(data) {
-	    $scope.movies = data;
-	  });
-//	$scope.movies = [
-//	                 {'title': 'Gone with the Wind', 'genre':'Romance, Drama', 'year':1939},
-//	                 {'title': 'The Matrix', 'genre':'Action, Sci-Fi', 'year':1999},
-//	                 {'title': 'Kill Bill: Vol. 1', 'genre':'Action, Crime', 'year':2003},
-//	                 {'title': 'The Bourne Identity', 'genre':'Action, Adventure, Mystery', 'year':2002}
-//	                 ];
+mediaApp.controller('MovieListCtrl', function($scope, $http, $filter, ngTableParams) {
+	$http.get('assets/movies.json').success(
+			function(data) {
+				$scope.movies = data;
 
+				$scope.tableParams = new ngTableParams({
+					page : 1,
+					count : 10,
+					sorting: {
+			            name: 'asc'
+			        }
+				// count per page
+				}, {
+					total : data.length, 
+					getData : function($defer, params) {
+						var orderedData = params.sorting() ? $filter('orderBy')(data, params.orderBy()) : data;
+						$defer.resolve(orderedData.slice((params.page() - 1)
+								* params.count(), params.page()
+								* params.count()));
+					}
+				});
+			});
 	$scope.movieProp = 'title';
-}); 
+});
