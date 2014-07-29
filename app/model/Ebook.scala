2fef6c1b1b8a1277647f5df92b606b4de046adc7
Jens Richnow <jens.richnow@adscale.co.nz>
2014-07-29 23:17:31 +1200

    expanded on query..

--- app/model/Ebook.scala
+++ app/model/Ebook.scala
@@ -9,17 +9,19 @@ import play.api.Play.current
 
 case class Ebook(
 	id: Long,
-	title: String) 
+	title: String,
+	isbn: Option[String]) 
 	
 object Ebook {
   
   val simple = {
     get[Long]("id") ~
-    get[String]("title") map {
-      case id ~ title => Ebook(id, title)
+    get[String]("title") ~
+    get[Option[String]]("val") map {
+      case id ~ title ~ isbn => Ebook(id, title, isbn)
     }
   }
   
   def findById(id:Long): Option[Ebook] = DB.withConnection {implicit connection => 
-    SQL("select id, title from books where id={id}").on('id -> id).as(simple.singleOpt) }
+    SQL("select books.id, title, type, val from books join identifiers on books.id = identifiers.book where books.id={id} and type='isbn'").on('id -> id).as(simple.singleOpt) }
 }
